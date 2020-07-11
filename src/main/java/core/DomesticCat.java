fbe70be2afb60fdd68c58e80f3780edf2072e8d3
vitaliynezhinskiy <SeekerOfConfessor@gmail.com>
2020-07-11 14:53:52 +0300

    Initial commit

---
+++ src/main/java/core/DomesticCat.java
@@ -0,0 +1,23 @@
+package core;
+
+import lombok.*;
+
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class DomesticCat {
+    private final String name;
+    private double weight;
+    private boolean isPretty;
+}
+
+
+//1. Создать 5 коров, у 3 из них поменять цвет
+//2. Написать set get для поля nameBreed
+//3. В методе main сменить породу и вывести на экран
+//4. Создать классы: Man, Shop, Cup, Telephone, Notebook и 3 своих
+//5. Создать классы для запуска
+//6. Создать get и set для всех полей
+//7. Создать конструкторы от разного числа параметров
+//Материалы: Брюс Эккель Философия Java
---
+++ src/main/java/core/Panther.java
@@ -0,0 +1,11 @@
+package core;
+
+public class Panther extends Cat {
+
+
+    @Override
+    public void sayMeow() {
+        System.out.println("Panther meow");
+    }
+
+}
